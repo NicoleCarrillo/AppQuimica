b2f7d1f7292e771e7c8b21051d459610512f5291
José Adolfo <jmicalco@gmail.com>
2020-10-31 16:44:08 -0600

    vanLaar y wilson

--- chemestry-app/src/clases/vanLaar.js
+++ chemestry-app/src/clases/vanLaar.js
@@ -1,22 +1,44 @@
-export default class vanLaar {
+/*export default class vanLaar {
   //sistema -> A12 A21
-  acetonaCloroformo = (-0.8643, -0.589);
-  acetonaMetanol = (0.6184, 0.5797);
-  acetonaAgua = (2.1041, 1.5555);
-  tetraBenceno = (0.0951, 0.0911);
-  cloroformoMetanol = (0.9356, 1.886);
-  etanolBenceno = (1.857, 1.4785);
-  etanolAgua = (1.6798, 0.9227);
-  acetatoetiloEtanol = (0.8552, 0.7526);
-  nhexanoEtanol = (1.9195, 2.8463);
-  metanolBenceno = (2.1623, 1.7925);
-  metanolAcetatoetilo = (1.0017, 1.0524);
-  metanolAgua = (0.8041, 0.5619);
-  acetatometiloMetanol = (0.9614, 1.0126);
-  propanol1Agua = (2.9095, 1.1572);
-  propanol2Agua = (2.4702, 1.0938);
-  tetrahidrofuranoAgua = (3.0216, 1.9436);
-  aguaAcidoacetico = (0.4973, 1.0623);
-  agua1Butanol = (1.0996, 4.176);
-  aguaAcidoformico = (-0.2935, -0.2757);
-}
+  acetonaCloroformo: [-0.8643, -0.589],
+  acetonaMetanol: [0.6184, 0.5797],
+  acetonaAgua : [2.1041, 1.5555],
+  tetraBenceno : [0.0951, 0.0911],
+  cloroformoMetanol : [0.9356, 1.886],
+  etanolBenceno : [1.857, 1.4785],
+  etanolAgua : [1.6798, 0.9227],
+  acetatoetiloEtanol : [0.8552, 0.7526],
+  nhexanoEtanol : [1.9195, 2.8463],
+  metanolBenceno : [2.1623, 1.7925],
+  metanolAcetatoetilo : [1.0017, 1.0524],
+  metanolAgua : [0.8041, 0.5619],
+  acetatometiloMetanol : [0.9614, 1.0126],
+  propanol1Agua : [2.9095, 1.1572],
+  propanol2Agua : [2.4702, 1.0938],
+  tetrahidrofuranoAgua : [3.0216, 1.9436],
+  aguaAcidoacetico : [0.4973, 1.0623],
+  agua1Butanol : [1.0996, 4.176],
+  aguaAcidoformico : [-0.2935, -0.2757],
+}*/
+const vanLaar = {
+  acetonaCloroformo: [-0.8643, -0.589],
+  acetonaMetanol: [0.6184, 0.5797],
+  acetonaAgua : [2.1041, 1.5555],
+  tetraBenceno : [0.0951, 0.0911],
+  cloroformoMetanol : [0.9356, 1.886],
+  etanolBenceno : [1.857, 1.4785],
+  etanolAgua : [1.6798, 0.9227],
+  acetatoetiloEtanol : [0.8552, 0.7526],
+  nhexanoEtanol : [1.9195, 2.8463],
+  metanolBenceno : [2.1623, 1.7925],
+  metanolAcetatoetilo : [1.0017, 1.0524],
+  metanolAgua : [0.8041, 0.5619],
+  acetatometiloMetanol : [0.9614, 1.0126],
+  propanol1Agua : [2.9095, 1.1572],
+  propanol2Agua : [2.4702, 1.0938],
+  tetrahidrofuranoAgua : [3.0216, 1.9436],
+  aguaAcidoacetico : [0.4973, 1.0623],
+  agua1Butanol : [1.0996, 4.176],
+  aguaAcidoformico : [-0.2935, -0.2757],
+};
+export default vanLaar;
