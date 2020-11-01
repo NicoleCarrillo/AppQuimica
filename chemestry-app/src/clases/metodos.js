5339580335d81bb18284b74662f0d50014c44320
Nicole Carrillo C <A01551581@itesm.mx>
2020-10-31 20:17:13 -0400

    paso 0 solucion ideal

---
+++ chemestry-app/src/clases/metodos.js
@@ -0,0 +1,11 @@
+import objetoConstantesAntoine from "../clases/constantesAntoine";
+
+const objeto = {
+    name1 = "acetona",
+    name2 = "cloroformo",
+    c_antoine1 = objetoConstantesAntoine.acetona,
+    c_antoine2 = objetoConstantesAntoine.cloroformo,
+    n = 11,
+    t_general = [0],
+};
+export default metodos;
